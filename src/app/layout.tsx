0f4e46107f6a71220893dd8d1c8d2fd4888fb488
kmounce82 <kmounce82@gmail.com>
2025-08-10 22:28:39 +0000

    modified src speed enable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from 'next'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={GeistMono.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
